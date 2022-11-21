ce9969aea6cf870b834aa3a4959b218ab2eb2520
Andrew <105199424+Jinnywoo@users.noreply.github.com>
2022-11-20 22:22:53 -0800

    created inquiries for employees

---
+++ index.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+cosnt fs = require("fs");
+
+
+const inqActions = {
+    type: 'list',
+    name: 'actions',
+    message: "Hello! Thank you for using Employee Tracker, To begin, select from the options below:"
+    choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update and employee's role"
+
+    ],
+}
